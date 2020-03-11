c1ccd3fd6c37a35dd3f2055947c5b94e8804b53e
wildintu <hello@wildintu.com>
2020-03-10 22:53:53 -0500

    postman working now

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,14 @@
 import * as express from 'express';
 import apiRouter from './routes';
+import chirps from './chirps'
 
 const app = express();
 
+app.use(express.json())
 app.use(express.static('public'));
 app.use(apiRouter);
+app.use(chirps);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
+

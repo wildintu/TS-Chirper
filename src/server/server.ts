38aa092a06da58fe23c73bf2114e3f1ceb2e6b3d
wildintu <hello@wildintu.com>
2020-03-23 10:23:49 -0500

    fixed path

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import apiRouter from './routes';
 import chirps from './chirps'
 import * as cors from 'cors';
+import * as path from 'path';
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(express.static('public'));
 app.use(apiRouter);
 app.use(chirps);
+app.get(["/","/Forms","/SingleChirp/:id/admin"], (req, res) => res.sendFile(path.join(__dirname, '../public', 'index.html')))
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
